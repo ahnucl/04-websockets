e4dda60db1debcf31cfb002a8a52c4006287d30c
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-11-13 14:40:14 +0000

    Cria template de pagamento

--- app.py
+++ app.py
@@ -46,8 +46,16 @@ def pix_confirmation():
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
-    # Payment.query(id=payment_id)
-    return render_template('payment.html')
+    payment = Payment.query.get(payment_id)
+    
+    if not payment:
+        return render_template('404.html')
+    
+    return render_template('payment.html', 
+                           payment_id=payment.id,
+                           value=payment.value, 
+                           host="http://localhost:5000",
+                           qr_code=payment.qr_code)
 
 
 if __name__ == '__main__':
