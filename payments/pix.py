f058aea2e91b598a90bd42d7ac249e93520a0c50
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-11-27 17:59:50 +0000

    Cria teste

--- payments/pix.py
+++ payments/pix.py
@@ -7,7 +7,7 @@ class Pix:
     def __init__(self):
         pass
     
-    def create_payment(self):
+    def create_payment(self, base_dir=""):
         # criar o pagamento na isntituição financeira
         # bank_payment_id = uuid.uuid4()
         bank_payment_id = str(uuid4())
@@ -19,7 +19,7 @@ class Pix:
         img = qrcode.make(hash_payment)
         
         # salvar imagem como arquivo png
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
         
         return {"bank_payment_id": bank_payment_id, 
                 "qr_code_path": f"qr_code_payment_{bank_payment_id}",}
